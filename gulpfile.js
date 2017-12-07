a1b78d8eea1586220c31ef676f905fe3722906c8
Boyan Mihaylov <hey@boyan.in>
2017-12-07 06:25:50 +0100

    Create wheel parts metadata on build, rather than dynamic on request

--- gulpfile.js
+++ gulpfile.js
@@ -17,27 +17,23 @@ const buildTasks = () => fs.readdirSync(`${__dirname}/src/langs`)
 
 gulp.task('build-wasm', buildTasks());
 
+gulp.task('build-metadata', ['build-wasm'], () => {
+    const wasmFiles = fs.readdirSync(`${__dirname}/build/wasm`)
+        .filter(file => file.startsWith('wheel-part') && file.endsWith('.wasm'))
+        .map(file => ({
+            file,
+            loader: fs.existsSync(`${__dirname}/build/wasm/${file}-loader.js`) ?
+                `${file}-loader.js` : null
+        }));
+
+    fs.writeFileSync(`${__dirname}/build/wheel-parts.json`, JSON.stringify({ wasmFiles }));
+});
+
+gulp.task('build', ['build-metadata']);
+
 gulp.task('http', (done) => {
     connect()
         .use(serveStatic(`${__dirname}/build`))
-        .use((req, res, next) => {
-            const url = req.url;
-            if (url.startsWith('/wheel-parts.json')) {
-                const wasmFiles = fs.readdirSync(`${__dirname}/build/wasm`)
-                    .filter(file => file.startsWith('wheel-part') && file.endsWith('.wasm'))
-                    .map(file => ({
-                        file,
-                        loader: fs.existsSync(`${__dirname}/build/wasm/${file}-loader.js`) ?
-                            `${file}-loader.js` : null
-                    }));
-
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.write(JSON.stringify({ wasmFiles }));
-                res.end();
-            } else {
-                next();
-            }
-        })
         .use((req, res, next) => {
             const url = req.url;
             console.log(url);
@@ -46,4 +42,4 @@ gulp.task('http', (done) => {
         .listen(8080, done);
 });
 
-gulp.task('default', ['build-wasm', 'http']);
+gulp.task('default', ['build', 'http']);
