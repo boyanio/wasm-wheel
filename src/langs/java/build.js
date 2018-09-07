32f73effad58206d8510850e3e0040dba4c680ca
Daniel Tschan <tschan@puzzle.ch>
2018-09-07 14:58:59 +0200

    Run Maven in batch mode.

--- src/langs/java/build.js
+++ src/langs/java/build.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 exports.task = (done) => {
     const buildDir = `${__dirname}/../../../build/wasm`;
 
-    const ls = exec('mvn clean install', { cwd: __dirname });
+    const ls = exec('mvn -B clean install', { cwd: __dirname });
     ls.stdout.pipe(process.stdout)
     ls.stderr.pipe(process.stdout)
     ls.on('exit', (code) => {
