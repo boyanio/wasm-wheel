868eb60e5876ccf4de6139edf58ce116a4bb4e43
MaxGraey <maxgraey@gmail.com>
2019-03-21 19:50:41 +0200

    fix opt-level

--- src/langs/rust/build.js
+++ src/langs/rust/build.js
@@ -5,7 +5,7 @@ const execp = require('../../execp');
 const copyFile = promisify(fs.copyFile);
 
 const buildWasm = async (buildDir) => {
-  await execp(`rustc --target wasm32-unknown-unknown -Oz -C lto --crate-type=cdylib -o ${buildDir}/wheel-part-rust.wasm ${__dirname}/wheel-part.rs`);
+  await execp(`rustc --target wasm32-unknown-unknown -C opt-level=z -C lto --crate-type=cdylib -o ${buildDir}/wheel-part-rust.wasm ${__dirname}/wheel-part.rs`);
 };
 
 const buildLoader = async (buildDir) => {
