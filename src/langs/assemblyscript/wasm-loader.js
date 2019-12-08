cceae57b1fbda726943d110f0584e82ef9016d02
MaxGraey <maxgraey@gmail.com>
2019-12-08 22:51:39 +0200

    update AssemblyScript

--- src/langs/assemblyscript/wasm-loader.js
+++ src/langs/assemblyscript/wasm-loader.js
@@ -1,30 +1,15 @@
 /* global wheel */
 (() => {
-  const getInt = (heap, offset) => {
-    return (
-      heap[offset + 0] <<  0 |
-      heap[offset + 1] <<  8 |
-      heap[offset + 2] << 16 |
-      heap[offset + 3] << 24
-    );
-  };
-
-  const getUshort = (heap, offset) => {
-    return heap[offset] | heap[offset + 1] << 8;
-  };
-
   const utf16leToString = (heap, offset) => {
-    const length = getInt(heap, offset);
-    const chars = new Array(length);
-    for (let i = 0; i < length; i++) {
-      chars[i] = getUshort(heap, offset + (i << 1) + 4);
-    }
-    return String.fromCharCode.apply(String, chars);
+    const U16 = new Uint16Array(heap);
+    const U32 = new Uint32Array(heap);
+    const length = U32[offset - 4];
+    return String.fromCharCode.apply(String, U16.subarray(offset, offset + length));
   };
 
   const importObject = {
     env: {
-      abort: () => {},
+      abort: console.error,
       random: Math.random
     },
   };
