5b4c83b87c28264a87a852e3c2b35a7fc28bbcd2
Boyan Mihaylov <boyan.mihailov@gmail.com>
2024-01-16 15:12:59 +0200

    Fix wasm build for C#

--- scripts/wasm.js
+++ scripts/wasm.js
@@ -5,6 +5,7 @@ const execp = require('./execp');
 
 const readdir = promisify(fs.readdir);
 const exists = promisify(fs.exists);
+const rmdir = promisify(fs.rmdir);
 
 const rootDir = path.resolve(__dirname, '../');
 const langsDir = path.resolve(rootDir, 'src/langs');
@@ -12,17 +13,20 @@ const langsDir = path.resolve(rootDir, 'src/langs');
 const buildWheelPart = async (lang) => {
   console.log(`\nBuilding ${lang} wheel part ...`);
 
-  await buildDocker(lang);
-};
-
-const buildDocker = async (lang) => {
   const langDir = path.resolve(langsDir, lang);
   if (await exists(path.resolve(langDir, 'Dockerfile'))) {
+    const outputDir = path.join(langDir, '/output');
+    if (await exists(outputDir)) {
+      await rmdir(outputDir, { recursive: true });
+    }
+
     const dockerBuildCmd = `docker build -t wheel-part-${lang}:latest .`;
     await execp(dockerBuildCmd, { cwd: langDir });
 
     const dockerRunCmd = `docker run --rm -v ${langDir}:/tmp wheel-part-${lang}:latest cp -r ../output ../tmp`;
     await execp(dockerRunCmd, { cwd: langDir });
+  } else {
+    console.warn(`No Dockerfile found for ${lang} wheel part.`);
   }
 };
 
