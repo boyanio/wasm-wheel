cc00e74fe7be17925d32d8111908d1ac481544e7
Frank Denis <github@pureftpd.org>
2024-01-29 23:53:20 +0100

    Zig: use wasm32-freestanding, no need for WASI

--- src/langs/zig/build.zig
+++ src/langs/zig/build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .wasi } });
+    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .freestanding } });
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSmall });
 
     const exe = b.addExecutable(.{
